57829174fae514640ad8e05d24c04f3381091bfb
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-05-21 22:53:20 -0300

    up

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,3 +1,4 @@
+import getCurrentUser from "@/actions/getCurrentUser";
 import getUsers from "@/actions/getUsers";
 import UserList from "@/components/UserList";
 import Sidebar from "@/components/sidebar/Sidebar";
@@ -8,13 +9,14 @@ export default async function UserLayout({
     children: React.ReactNode;
 }) {
     const users = await getUsers()
+    const currentUser = await getCurrentUser()
 
     return (
 
         // @ts-expect-error Server Component
         <Sidebar>
             <div className="h-full">
-                <UserList items={users} />
+                <UserList items={users} user={currentUser}/>
                 {children}
             </div>
         </Sidebar>
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,53 +2,81 @@
 
 import { User } from '@prisma/client'
 import UserBox from './UserBox'
+import Avatar from './Avatar'
+import { useState } from 'react'
+import SettingsModal from './sidebar/SettingsModal'
+import { getSession } from 'next-auth/react'
 
 interface UserListProps {
-    items: User[]
+    items: User[];
+    user?: User | null;
 }
 
 const UserList: React.FC<UserListProps> = ({
-    items
+    items,
+    user
 }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+
     return (
-        <aside
-            className='
-                fixed
-                inset-y-0
-                pb-20
-                md:pb-0
-                md:left-20
-                md:w-80
-                md:block
-                overflow-y-auto
-                border-r
-                border-gray-200
-                block
-                w-full
-                left-0
-            '
-        >
-            <div className='px-5'>
-                <div className='flex-col'>
-                    <div
-                        className='
-                            text-2xl
-                            font-bold
-                            text-neutral-800
-                            py-4
-                        '
-                    >
-                        People
+        <>
+            <SettingsModal 
+                currentUser={user!}
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+            />
+            <aside
+                className='
+                    fixed
+                    inset-y-0
+                    pb-20
+                    md:pb-0
+                    md:left-20
+                    md:w-80
+                    md:block
+                    overflow-y-auto
+                    border-r
+                    border-gray-200
+                    block
+                    w-full
+                    left-0
+                '
+            >
+                <div className='px-5'>
+                    <div className='flex justify-between items-center'>
+                        <div
+                            className='
+                                text-2xl
+                                font-bold
+                                text-neutral-800
+                                py-4
+                            '
+                        >
+                            People
+                        </div>
+                        <div
+                            onClick={() => setIsOpen(true)}
+                            className="
+                                md:hidden
+                                cursor-pointer
+                                hover:opacity-75
+                                transition
+                                mt-2
+                            "
+                        >
+                            <Avatar user={user!} />
+                        </div>
                     </div>
+                    {items.map((item) => (
+                        <UserBox 
+                            key={item.id}
+                            data={item}
+                        />
+                    ))}
                 </div>
-                {items.map((item) => (
-                    <UserBox 
-                        key={item.id}
-                        data={item}
-                    />
-                ))}
-            </div>
-        </aside>
+            </aside>
+        </>
     )
 }
 export default UserList;
